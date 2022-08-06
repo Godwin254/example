1e4da462b6c20e377db737fbcd9c747b3b5761f3
Godwin254 <godwinbee15@gmail.com>
2022-08-06 18:14:11 +0300

    modify styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BsPlayCircle, BsFillPlayFill } from 'react-icons/bs'
+import { AiOutlinePlus, AiFillStar } from 'react-icons/ai'
 
 export default function Home({movies, handleSelected, selected}) {
 
@@ -7,6 +9,7 @@ export default function Home({movies, handleSelected, selected}) {
   return (
     <div className="home">
         <DisplayMovie />
+        <BsPlayCircle className="main-player" />
         <div className='lists'>
             {
                 movies.map(movie => <Movie key={movie.id} movie={movie} handleSelected={handleSelected} selected ={selected} />)
@@ -20,9 +23,7 @@ export default function Home({movies, handleSelected, selected}) {
 
 
 const DisplayMovie = ({ selectedMovie }) => {
-
     //const {title, type, rating, genres, plot, url} = selectedMovie;
-
     return (
         <>
             <img src={require("../images/image3.jpg")} alt="image1" />
@@ -31,13 +32,25 @@ const DisplayMovie = ({ selectedMovie }) => {
                 <h1>The Witcher</h1>
                 <span>Season 1</span>
                 <div className="rating">
-                    <span>Rating- {8.5}</span>
+                    <AiFillStar className="star" />
+                    <AiFillStar className="star" />
+                    <AiFillStar className="star" />
+                    <AiFillStar className="star" />
+                    <AiFillStar className="star" />
                 </div>
                 <div className='genres'>
                     <span>Action</span>
                     <span>Adventure</span>
+                    <span>Horror</span>
                 </div>
-                <p>The Witcher is a series of video games developed and published by CD Projekt RED and later converted into a film series by Polish studio Obsidian Entertainment. The series is based on the graphic novel The Witcher: Wild Hunt, and was released in multiple regions worldwide on October 24, 2017, and concluded with the film The Witcher: The Game of the Year 2017 on January 31, 2018.</p>
+                <div className="controls">
+                    <BsFillPlayFill className="play ctrl" />
+                    <AiOutlinePlus className="plus ctrl" />
+                </div>
+                <p>
+                    The Witcher is a series of video games developed and published by CD Projekt RED and later converted into a film series by Polish studio Obsidian Entertainment. The series is based on the graphic novel The Witcher: Wild Hunt, and was released in multiple regions worldwide on October 24, 2017, and concluded with the film The Witcher: The Game of the Year 2017 on January 31, 2018.
+
+                </p>
             </div>
         </>
     )
@@ -50,7 +63,7 @@ const Movie = ({movie, handleSelected, selected}) => {
     const {url, title} = movie;
 
     return (
-        <div className='movie' onClick={handleSelected} style={{border: selected ? "1px solid" : "none"}}>
+        <div className='movie' onClick={handleSelected} >
             <img src={url} />
             <span>{title}</span>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {  BsSearch } from 'react-icons/bs'
+import { MdOutlineFavoriteBorder, MdOutlineAccountCircle } from 'react-icons/md'
+import { RiMovie2Line } from 'react-icons/ri'
 
 export default function Navbar() {
   return (
     <div className="navbar">
         <div className='logo'>
-            <Link to='/'><h1>Moovizz</h1></Link>
+        <Link to='/'><h1><RiMovie2Line className='lg'/>moviz</h1></Link>
         </div>
         <div className='nav-links'>
             <Link to="/">Home</Link>
@@ -14,8 +17,10 @@ export default function Navbar() {
             <Link to="/add-review">Add Review</Link>
         </div>
 
-        <div className='search'>
-            s
+        <div className='btns'>
+          <MdOutlineFavoriteBorder className="btn favorite" />
+          <MdOutlineAccountCircle className="btn account" />
+          <BsSearch className='btn search' />
         </div>
     </div>
   )
